6c6221f78258a3a26f13eaef3791adfdc0071718
Vladislav <vladislav_sheshko@mail.ru>
2026-04-09 09:31:02 +0500

    Сохранение сгенерированных данных в json и создание групп на основе данных из файла

--- addressbook-web-tests/src/test/java/generator/Generator.java
+++ addressbook-web-tests/src/test/java/generator/Generator.java
@@ -4,7 +4,11 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import common.CommonFunctions;
 import model.GroupData;
+import tools.jackson.databind.SerializationFeature;
+import tools.jackson.databind.json.JsonMapper;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -22,7 +26,7 @@ public class Generator {
     @Parameter(names = {"--count", "-n"})
     int count;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var generator = new Generator();
         JCommander.newBuilder()
                 .addObject(generator)
@@ -31,11 +35,11 @@ public class Generator {
         generator.run();
     }
 
-    private void run() {
-        var data = generate();
+    private void run() throws IOException {
+        var data = generateGroups();
         save(data);
     }
-
+/*
     private Objects generate() {
         if ("groups".equals(type)) {
             return generateGroups();
@@ -45,8 +49,8 @@ public class Generator {
             throw new IllegalArgumentException("Неизвестный тип данных " + type);
         }
     }
-
-    private Objects generateGroups() {
+*/
+    private ArrayList<GroupData> generateGroups() {
         var result = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
             result.add(new GroupData()
@@ -61,6 +65,14 @@ public class Generator {
         return null;
     }
 
-    private void save(Objects data) {
+    private void save(ArrayList<GroupData> data) throws IOException {
+        if ("json".equals(format)) {
+            JsonMapper.builder()
+                    .enable(SerializationFeature.INDENT_OUTPUT)
+                    .build()
+                    .writeValue(new File(output), data);
+        } else {
+            throw new IllegalArgumentException("Неизвестный формат данных " + format);
+        }
     }
 }
