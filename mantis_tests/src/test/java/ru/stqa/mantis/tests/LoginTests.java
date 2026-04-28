fe8490db421e442cbc6be4978bc26f1317628136
Vladislav <vladislav_sheshko@mail.ru>
2026-04-28 10:36:25 +0500

    Реализован тест проверяющий логирование пользователя через http

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -5,9 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class LoginTests extends TestBase {
 
+    //тест через UI
     @Test
-    void canLogin() {
+    void canLoginUi() {
         app.session().login("administrator", "root");
         Assertions.assertTrue(app.session().isLoggedIn());
+    }
+
+    //тест через http
+    @Test
+    void canLoginHttp() {
+        app.http().login("administrator", "root");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
