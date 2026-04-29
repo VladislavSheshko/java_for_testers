83bac5499885389f1ba94db071d422e775adc6fe
Vladislav <vladislav_sheshko@mail.ru>
2026-04-29 11:27:24 +0500

    Реализован тест по добавлению нового почтового email

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
@@ -0,0 +1,39 @@
+package ru.stqa.mantis.manager;
+
+
+import org.openqa.selenium.io.CircularOutputStream;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JamesCliHelper extends HelperBase {
+
+    public JamesCliHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+    public void addUser(String email, String password) {
+        try {
+            new ProcessBuilder("java", "-cp", "james-server-jpa-app.lib/*",
+                    "org.apache.james.cli.ServerCmd", "AddUser", email, password)
+                    .directory(new File(manager.property("james.workingDir")))
+                    .redirectErrorStream(true)
+                    .inheritIO()
+                    .start()
+                    .waitFor();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+//
+//CommandLine cmd = new CommandLine(
+//        "java", "-cp", "\"james-server-jpa-app.lib/*\"",
+//        "org.apache.james.cli.ServerCmd",
+//        "AddUser", email, password);
+//cmd.setWorkingDirectory(manager.property("james.workingDir"))
+//CircularOutputStream out = new CircularOutputStream();
+//        cmd.copyOutputTo(out);
+//        cmd.execute();
+//        cmd.waiFor();
+//        System.out.println(out);
