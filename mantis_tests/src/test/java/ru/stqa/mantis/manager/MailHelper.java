d7028b6ce499666724983b5d985c33f23cd128a4
Vladislav <vladislav_sheshko@mail.ru>
2026-05-01 12:18:26 +0500

    Добавлен код по извлечению ссылки из почты

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
@@ -1,12 +1,10 @@
 package ru.stqa.mantis.manager;
 
-import jakarta.mail.Folder;
-import jakarta.mail.MessagingException;
-import jakarta.mail.Session;
-import jakarta.mail.Store;
+import jakarta.mail.*;
 import ru.stqa.mantis.model.MailMessage;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -17,12 +15,10 @@ public class MailHelper extends HelperBase {
         super(manager);
     }
 
+    //метод без ожидания
     public List<MailMessage> receive(String username, String password) {
         try {
-            Session session = Session.getInstance(new Properties());
-            Store store = session.getStore("pop3");
-            store.connect("localhost",username, password);
-            var inbox = store.getFolder("INBOX");
+            var inbox = getInbox(username, password);
             inbox.open(Folder.READ_ONLY);
             var messages = inbox.getMessages();
             var result = Arrays.stream(messages)
@@ -37,8 +33,74 @@ public class MailHelper extends HelperBase {
                     })
                     .toList();
             inbox.close();
-            store.close();
+            inbox.getStore().close();
             return result;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //метод с ожиданием
+    public List<MailMessage> receive(String username, String password, Duration duration) {
+        var start = System.currentTimeMillis();
+        while (System.currentTimeMillis() < start + duration.toMillis()) {
+            try {
+                var inbox = getInbox(username, password);
+                inbox.open(Folder.READ_ONLY);
+                var messages = inbox.getMessages();
+                var result = Arrays.stream(messages)
+                        .map(m -> {
+                            try {
+                                return new MailMessage()
+                                        .withFrom(m.getFrom()[0].toString())
+                                        .withContent((String) m.getContent());
+                            } catch (MessagingException | IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .toList();
+                inbox.close();
+                inbox.getStore().close();
+                if (result.size() > 0) {
+                    return result;
+                }
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new RuntimeException("No mail");
+    }
+
+    private static Folder getInbox(String username, String password) {
+        try {
+            Session session = Session.getInstance(new Properties());
+            Store store = session.getStore("pop3");
+            store.connect("localhost", username, password);
+            var inbox = store.getFolder("INBOX");
+            return inbox;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void drain(String username, String password) {
+        var inbox = getInbox(username, password);
+        try {
+            inbox.open(Folder.READ_WRITE);
+            Arrays.stream(inbox.getMessages()).forEach(m -> {
+                try {
+                    m.setFlag(Flags.Flag.DELETED, true);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            inbox.close();
+            inbox.getStore().close();
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
@@ -3,12 +3,43 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.regex.Pattern;
+
 public class MailTests extends TestBase {
 
+    //получаем почту без ожидания
     @Test
     void canReceiveEmail() {
         var messages = app.mail().receive("user1@localhost", "password");
         Assertions.assertEquals(1, messages.size());
         System.out.println(messages);
+    }
+
+    //получаем почту с ожиданием
+    @Test
+    void canReceiveEmail2() {
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
+        Assertions.assertEquals(1, messages.size());
+        System.out.println(messages);
+    }
+
+    //удаление почтового адреса
+    @Test
+    void canDrainInbox() {
+        app.mail().drain("user1@localhost", "password");
+    }
+
+    //извлечение ссылки из письма
+    @Test
+    void canExtractUrl() {
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S*");
+        var matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            var url = text.substring(matcher.start(), matcher.end());
+            System.out.println(url);
+        }
     }
 }
