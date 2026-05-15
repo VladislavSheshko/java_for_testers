c3ea1c2b0bd1349c6a5dadf7fd482b86184ead68
Vladislav <vladislav_sheshko@mail.ru>
2026-05-15 11:14:13 +0500

    Исправлен тест canRegisterUser, чтобы данные передавались через параметры

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -1,10 +1,13 @@
 package ru.stqa.mantis.tests;
 
 import org.junit.jupiter.api.Test;
+import ru.stqa.mantis.common.CommonFunctions;
 import ru.stqa.mantis.model.AccountData;
 
 import java.time.Duration;
 import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserRegistrationTests extends TestBase {
 
@@ -12,31 +15,31 @@ public class UserRegistrationTests extends TestBase {
     //используется jamesCli
     @Test
     void canRegisterUser() {
-        //var email = String.format("%s@localhost", username);
+        var userName = CommonFunctions.randomString(5);
+        var password = "password";
+        var emailDomain = "localhost";
+        var email = String.format("%s@%s", userName, emailDomain);
         // авторизоваться через UI
         app.session().login("administrator", "root");
         // создать пользователя (адрес) на почтовом сервере (JamesHelper)
-        app.jamesCli().addUser("8@localhost",
-                "password");
+        app.jamesCli().addUser(email, password);
         // заполняем форму создания и отправляем (браузер)
-        var account = new AccountData("Vlad2", "Sheshko", "8@localhost", "", true, false);
+        var account = new AccountData(userName, "Sheshko", email, "", true, false);
         app.account().createAccount(account);
         // ждем почту (MailHelper)
-        var messages = app.mail().receive("8@localhost", "password", Duration.ofSeconds(10));
+        var messages = app.mail().receive(email, password, Duration.ofSeconds(10));
         // извлекаем ссылку из письма
         var text = messages.get(0).content();
         var pattern = Pattern.compile("http://\\S*");
         var matcher = pattern.matcher(text);
-        String url = "";
-        if (matcher.find()) {
-            url = text.substring(matcher.start(), matcher.end());
-            //System.out.println(url);
-        }
+        assertTrue(matcher.find(), "Confirmation link not found in email");
+
+        var url = matcher.group();
         // проходим по ссылке и завершаем регистрацию (браузер)
         app.driver().get(url);
         app.account().accountActivation();
         // проверяем, что пользователь может залогиниться (HttpSessionHelper)
-        app.session().login("Vlad2", "password");
+        app.session().login(userName, password);
     }
 
     //Используется REST API в создание почты и аккаунта
